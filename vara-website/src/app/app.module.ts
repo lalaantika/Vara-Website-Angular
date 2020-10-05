376bd559403fc73be5113eef3dbbcd358ad68dd3
lalaantika <sharmal0707@gmail.com>
2020-10-05 09:46:59 -0400

    intial commit

---
+++ vara-website/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { TechSupportComponent } from './tech-support/tech-support.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    AboutUsComponent,
+    TeamComponent,
+    ContactComponent,
+    TechSupportComponent,
+    FooterComponent,
+    LoginComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
