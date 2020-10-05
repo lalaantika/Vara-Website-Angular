4f5819ff2ffd216282cfea3371c3c145998d2435
lalaantika <sharmal0707@gmail.com>
2020-10-05 15:22:44 -0400

    some changes

--- vara-website/src/environments/environment.ts
+++ vara-website/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAk8AwKvL4wJ2JOj2-ctsZS0gSYFg5uvPQ",
+    authDomain: "vara-21be0.firebaseapp.com",
+    databaseURL: "https://vara-21be0.firebaseio.com",
+    projectId: "vara-21be0",
+    storageBucket: "vara-21be0.appspot.com",
+    messagingSenderId: "1008556245213",
+    appId: "1:1008556245213:web:45a9e876022ba762509b34",
+    measurementId: "G-2ZYE3QKYWY"
+  }
 };
 
 /*
