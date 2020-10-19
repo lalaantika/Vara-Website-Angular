de51ed9a560e35ab38c1a833e01155a8b6caca1c
lalaantika <sharmal0707@gmail.com>
2020-10-19 10:40:25 -0400

    login works

--- vara-website/src/app/services/user-authservice.service.ts
+++ vara-website/src/app/services/user-authservice.service.ts
@@ -1,10 +1,57 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { User } from "../services/user";
+import{ AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { auth } from 'firebase/app';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserAuthserviceService {
+  user: User;
+  constructor( 
+    public afs: AngularFirestore, 
+    public afAuth: AngularFireAuth, 
+    public router:Router) {
+      this.afAuth.authState.subscribe(user => {
+        if (user){
+          this.user = user;
+          localStorage.setItem('user', JSON.stringify(this.user));
+        } else {
+          localStorage.setItem('user', null);
+        }
+    })
+   }
 
-  constructor() { }
+  async login(email: string, password: string) {
+  var result = await this.afAuth.signInWithEmailAndPassword(email, password)
+  this.router.navigate(['home']);
+  }
+
+  async register(email: string, password: string) {
+    var result = await this.afAuth.createUserWithEmailAndPassword(email, password)
+    this.sendEmailVerification();
+  }
+
+  async sendEmailVerification() {
+    await (await this.afAuth.currentUser).sendEmailVerification()
+    this.router.navigate(['admin/verify-email']);
+  }
+
+  async sendPasswordResetEmail(passwordResetEmail: string) {
+    return await this.afAuth.sendPasswordResetEmail(passwordResetEmail);
+ }
+
+  async logout(){
+    await this.afAuth.signOut();
+    localStorage.removeItem('user');
+    this.router.navigate(['admin/login']);
+  }
+
+  get isLoggedIn(): boolean {
+    const  user  =  JSON.parse(localStorage.getItem('user'));
+    return  user  !==  null;
+  }
 
 }
