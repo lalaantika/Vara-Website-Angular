e68b32b4e18af3beeafed9084930daf4c38a1b09
Gervan Nantel <gervann@yahoo.ca>
2020-12-29 14:19:25 -0500

    did update

--- vara-website/src/app/tech-support/tech-support.component.spec.ts
+++ vara-website/src/app/tech-support/tech-support.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TechSupportComponent } from './tech-support.component';
 
@@ -6,7 +6,7 @@ describe('TechSupportComponent', () => {
   let component: TechSupportComponent;
   let fixture: ComponentFixture<TechSupportComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TechSupportComponent ]
     })
